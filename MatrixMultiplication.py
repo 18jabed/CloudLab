6fd53441ec403ec07c7f60a1405cdba792c5240b
18jabed <123971906+18jabed@users.noreply.github.com>
2023-06-12 14:49:19 +0600

    Update MatrixMultiplication.py

--- MatrixMultiplication.py
+++ MatrixMultiplication.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     # Matrix A: 2x3
-    A = [[1, 2, 3],
-         [4, 5, 6]]
+    A = [[1, 5, 3],
+         [4, 8, 6]]
 
     # Matrix B: 3x2
     B = [[7, 8],
          [9, 10],
-         [11, 12]]
+         [11, 13]]
 
     # Result matrix: 2x2
     result = [[0, 0],
